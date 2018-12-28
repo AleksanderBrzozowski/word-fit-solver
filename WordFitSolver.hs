4a7797e17c13cd5682aa06dd784a410881f42668
Aleksander Brzozowski <abrzozowski@apreel.com>
2018-12-28 23:52:48 +0100

    matrix data -> type

--- WordFitSolver.hs
+++ WordFitSolver.hs
@@ -10,10 +10,10 @@ import Data.List
 
 data Point = Point Int Int deriving (Eq, Show)
 data Cell = Cell Char Point deriving (Eq, Show)
-data Matrix = Matrix [[Cell]] deriving (Show)
+type Matrix = [[Cell]]
 
 matrix :: [[Char]] -> Matrix
-matrix rows = Matrix (zipWith (\row rowIndex -> matrixCols row rowIndex) rows [0..])
+matrix rows = zipWith (\row rowIndex -> matrixCols row rowIndex) rows [0..]
     where
         matrixCols :: [Char] -> Int -> [Cell]
         matrixCols row rowIndex = zipWith (\letter colIndex -> Cell letter (Point rowIndex colIndex)) row [0..]
@@ -22,10 +22,10 @@ combinations :: Matrix -> [[Cell]]
 combinations m = rows m
     where
         rows :: Matrix -> [[Cell]]
-        rows (Matrix m) = m
+        rows m = m
 
 printSolution :: Matrix -> [Cell] -> IO()
-printSolution (Matrix m) excludedCells = mapM_ (\x -> putStrLn $ row x excludedCells) m
+printSolution m excludedCells = mapM_ (\x -> putStrLn $ row x excludedCells) m
     where
         row :: [Cell] -> [Cell] -> [Char]
         row rowCells excludedCells = map (\c -> if (elem c excludedCells) then ' ' else cellLetter c) rowCells
