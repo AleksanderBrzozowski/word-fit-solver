9ee669a88f4fcc5899d7652141b742c8e728dc2c
Aleksander Brzozowski <abrzozowski@apreel.com>
2018-12-29 23:40:23 +0100

    combinations

--- WordFitSolver.hs
+++ WordFitSolver.hs
@@ -4,34 +4,72 @@ module WordFitSolver (
     matrix,
     combinations,
     printSolution,
+    matchedPoints,
+    printMatrix,
 ) where
 
 import Data.List
 
-data Point = Point Int Int deriving (Eq, Show)
-data Cell = Cell Char Point deriving (Eq, Show)
+type Point = (Int, Int)
+type Cell = (Char, Point)
 type Matrix = [[Cell]]
 
 matrix :: [[Char]] -> Matrix
 matrix rows = zipWith (\row rowIndex -> matrixCols row rowIndex) rows [0..]
     where
         matrixCols :: [Char] -> Int -> [Cell]
-        matrixCols row rowIndex = zipWith (\letter colIndex -> Cell letter (Point rowIndex colIndex)) row [0..]
+        matrixCols row rowIndex = zipWith (\letter colIndex -> (letter, (rowIndex, colIndex))) row [0..]
 
-combinations :: Matrix -> [[Cell]]
-combinations m = rows m
+combinations :: Matrix -> [Cell]
+combinations m = rows m ++ cols m ++ diagonals m
+
+rows :: Matrix -> [Cell]
+rows m = concat m
+
+cols :: Matrix -> [Cell]
+cols ([]:_) = []
+cols m = map head m ++ cols (map tail m)
+
+diagonals :: Matrix -> [Cell]
+diagonals m = diagonals' m ++ diagonals' (invert m)
     where
-        rows :: Matrix -> [[Cell]]
-        rows m = m
+        invert :: Matrix -> Matrix
+        invert []     = []
+        invert (x:xs) = (invert xs) ++ [x]
+        diagonals' :: Matrix -> [Cell]
+        diagonals' m = main m ++ downTriangle m ++ upTriangle m
+            where
+                main :: Matrix -> [Cell]
+                main [] = []
+                main ([]:_) = []
+                main (x:xs)  = head x : main (map tail xs)
+                downTriangle :: Matrix -> [Cell]
+                downTriangle [] = []
+                downTriangle (r:rs) = main rs ++ downTriangle rs
+                upTriangle :: Matrix -> [Cell]
+                upTriangle m = upTriangle' (map tail m)
+                    where
+                        upTriangle' :: Matrix -> [Cell]
+                        upTriangle' m | length (head m) == 1 = head m
+                                      | otherwise = main m ++ upTriangle' (map tail m)
 
-printSolution :: Matrix -> [Cell] -> IO()
-printSolution m excludedCells = mapM_ (\x -> putStrLn $ row x excludedCells) m
+matchedPoints :: [Cell] -> [Char] -> [Point]
+matchedPoints [] _ = []
+matchedPoints (w:ws) pattern = matchedPoints' (w:ws) pattern [] ++ (matchedPoints ws pattern)
     where
-        row :: [Cell] -> [Cell] -> [Char]
-        row rowCells excludedCells = map (\c -> if (elem c excludedCells) then ' ' else cellLetter c) rowCells
-        cellLetter :: Cell -> Char
-        cellLetter (Cell c p) = c
+        matchedPoints' :: [Cell] -> [Char] -> [Point] -> [Point]
+        matchedPoints' _ [] points = points
+        matchedPoints' [] _ points = []
+        matchedPoints' ((c, point):ws) (p:ps) points | c == p = matchedPoints' ws ps (points ++ [point])
+                                                     | otherwise = [] 
 
--- printMatrix :: Matrix -> IO()
--- printMatrix m = mapM_ (\x -> putStrLn $ row x excludedCells) m
+
+printSolution :: Matrix -> [Point] -> IO()
+printSolution m excludedPoints = mapM_ (\x -> putStrLn $ row x excludedPoints) m
+    where
+        row :: [Cell] -> [Point] -> [Char]
+        row rowCells excludedPoints = map (\(c,p) -> if (elem p excludedPoints) then ' ' else c) rowCells
+
+printMatrix :: Matrix -> IO()
+printMatrix m = mapM_ (\x -> putStrLn (map(\(c, _) -> c) x)) m
     
--- main.hs
+++ main.hs
@@ -1,6 +1,64 @@
 import WordFitSolver
 
 main = do
-    let m = matrix(["ABC","DEF","GHI"])
-    let combination = combinations m
-    printSolution m [(Cell 'A' (Point 0 0))]
+    let m = matrix [
+         "JULIETCARESS",
+         "WEDDINGROMEO",
+         "CHOCOLATERGF",
+         "BOUQUETTESRI",
+         "SWEETSLRUTOR",
+         "PEVETUOONEOS",
+         "BONNEDVNRAMT",
+         "LRESAZOYGRTK",
+         "RCIMEILSDRTI",
+         "SIEDTQGUAORS",
+         "CRNOEUUECWIS",
+         "AEVGHNHEEKSD",
+         "REEMBRACEETA",
+         "DATINGSRSNAT",
+         "SCOUPLEOYSNE",
+         "CUPIDPRESENT"]
+    let words = [
+         "ADORER",
+         "LYRE",
+         "ARROW",
+         "ODE",
+         "BOUQUET",
+         "POEM",
+         "BRIDE",
+         "PRESENT",
+         "CARDS",
+         "QUEEN",
+         "CARESS",
+         "RENDEZVOUS",
+         "CHOCOLATE",
+         "RING",
+         "COUPLE",
+         "ROMEO",
+         "CUPID",
+         "ROSES",
+         "DATE",
+         "RYE",
+         "DATING",
+         "SCENTS",
+         "DEVOTION",
+         "SENSE",
+         "EMBRACE",
+         "SONG",
+         "FIRSTKISS",
+         "SWEET",
+         "GROOM",
+         "TRISTAN",
+         "HEART",
+         "WEDDING",
+         "HUGS",
+         "ISEULT",
+         "JULIET",
+         "LOVE",
+         "LUCK"  ]
+    let matrixCombinations = combinations m
+    let cells = concat (map (\word -> matchedPoints matrixCombinations word) words)
+    putStrLn "Matrix:\n"
+    printMatrix m
+    putStrLn "\nSolution:\n"
+    printSolution m cells
